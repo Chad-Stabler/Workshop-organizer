4c0e92921aec7962431408e370eee318cfc2fa94
Chad Stabler <stablerpsn@gmail.com>
2022-05-25 11:33:37 -0400

    cleaning test bug

--- test/example.test.js
+++ test/example.test.js
@@ -6,13 +6,13 @@ const test = QUnit.test;
 test('time to test a function', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = '';
+    const expected = true;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = '';
+    const actual = true;
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual.outerHTML, expected);
+    expect.equal(actual, expected);
 });
